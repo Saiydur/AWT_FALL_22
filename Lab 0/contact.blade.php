e9179b05e01adff8c91c812b5084ea8664d6707f
Sayidur <saiydurrahman28081999@gmail.com>
2022-09-30 00:55:38 +0600

    Adding Lab 0 Practice Files

---
+++ Lab 0/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', $title)
+
+@section('content')
+    <h1>{{ $title }}</h1>
+@endsection
---
+++ Lab 0/main.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.app")
+
+@section("title", $title)
+
+@section("content")
+    <h1>{{ $title }} This is main page</h1>
+@endsection
